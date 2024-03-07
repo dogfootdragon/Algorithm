2034b733d375e5083fc152aa45c44d28a35ea231
dogfootdragon <dogfootdragon@gmail.com>
2024-03-07 22:58:55 +0900

    lv1 소수찾기

--- programmers/lv1/lv1.js
+++ programmers/lv1/lv1.js
@@ -53,3 +53,23 @@ function solution(participant, completion) {
 
     return answer;
 }
+
+//소수 찾기
+function solution(n) {
+    var answer = [];
+    //~N까지 들어간 배열 만들기
+    let arr = Array.from({length:n+1},(v,i)=>i); // [0,1,2,3,4,5...10]
+
+    //2부터 n의 제곱근까지 반복문. 해당 숫자 i의 배수들을 0으로 만든다(추후 필터로 삭제하기 위해)
+    for(let i=2; i<=parseInt(Math.sqrt(n)); i++){
+        //i가 2일때. 2의 배수를 지워야 함 > 해당 반복문의 j초기값은(2*2=4), 반복문이 끝나면 4+i(2)=6... 6+2=8... 8+2=10... 2의 배수들만 반복하게 됨.
+        for(let j=i*i; j<=n; j+=i){
+            arr[j] = 0;
+        }
+    }
+
+    //1과 0을 걸러내고 남은 배열의 길이를 리턴한다
+    answer = arr.filter((el)=>el!==0); //0 삭제
+    answer.shift(); //1 삭제
+    return answer.length;
+}

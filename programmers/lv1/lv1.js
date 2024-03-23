57f85c46dd7e53626ab4ac388006908e94f32d49
dogfootdragon <dogfootdragon@gmail.com>
2024-03-23 22:22:15 +0900

    lv1 행렬의 덧셈

--- programmers/lv1/lv1.js
+++ programmers/lv1/lv1.js
@@ -266,3 +266,16 @@ function solution(s) {
         return false;
     }
 }
+
+
+// 행렬의 덧셈
+function solution(arr1, arr2) {
+    var answer = [[]];
+    for (var i=0; i<arr1.length; i++){
+        answer[i] =[];
+        for(var j=0; j<arr1[i].length; j++){
+            answer[i].push(arr1[i][j] + arr2[i][j]);
+        }
+    }
+    return answer;
+}
